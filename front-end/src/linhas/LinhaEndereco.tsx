a93e30ae5f8afeb13965926a9cc081541689ea28
Tiago Amaral <tiagoamral23@gmail.com>
2024-11-13 19:47:28 -0300

    Add linha endereço

---
+++ front-end/src/linhas/LinhaEndereco.tsx
@@ -0,0 +1,15 @@
+import { startCase, toLower } from 'lodash'
+export default function LinhaEndenreco(props: any) {
+    let logradouro = startCase(toLower(props.endereco.logradouro));
+    let municipio = startCase(toLower(props.endereco.municipio));
+    let bairro = startCase(toLower(props.endereco.bairro));
+    let numero = props.endereco.numero;
+    let cep = props.endereco.cep;
+    let uf = props.endereco.uf;
+    let endereco = `${logradouro}, ${numero} - ${bairro}, ${municipio} - ${uf}, ${cep}`
+    return (
+      <div className="text-center">
+        <p className="">{endereco}</p>
+      </div>
+    )
+  }
