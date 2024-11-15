11fde4c9754f898bb7ee7b36058a8d38274b791d
Tiago Amaral <tiagoamral23@gmail.com>
2024-11-15 18:59:45 -0300

    Add linha idade

---
+++ front-end/src/linhas/LinhaIdade.tsx
@@ -0,0 +1,11 @@
+import { differenceInYears, parse } from 'date-fns'
+export default function LinhaIdade(props: any) {
+    let data_abertura = parse(props.data_abertura, "dd/MM/yyyy", new Date());
+    let idade = differenceInYears(new Date(), data_abertura);
+    let linha = `${idade} anos de existência`
+    return (
+      <div className="text-center">
+        <p className="">{linha}</p>
+      </div>
+    )
+  }
